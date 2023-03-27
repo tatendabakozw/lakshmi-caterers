78d26971bd6130fa5f68cc826b41eb13f2d399a2
tatendabakozw <tatendabakozw@gmail.com>
2023-03-27 13:59:27 +0200

    featureL added testimonials

--- src/components/page-sections/TestimonialsSection.tsx
+++ src/components/page-sections/TestimonialsSection.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const TestimonialsSection = (props: Props) => {
   return (
     <div className=" bg-slate-100 w-full py-16 flex-wrap">
-        <p className="capitalize pb-2 text-slate-900 text-3xl font-semibold text-center">Why out Customers Love us</p>
+        <p className="capitalize pb-2 text-slate-900 text-3xl font-semibold text-center">Why our Customers Love us</p>
         <p className="pb-16 text-slate-500 text-center ">Get a few testimonials from our customers on why our services are great</p>
       <div className="max-w-7xl w-full mx-auto px-2 grid md:grid-cols-4 grid-cols-1 items-center gap-8 justify-around">
         {[1, 2, 3, 4, 5].map((item, index) => (
